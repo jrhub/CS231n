c0edba00c91bc059ee489badcedf5bb9115fba04
Jie Ren <jieren@ksu.edu>
2019-07-08 00:27:29 -0500

    softmax

--- assignment1/cs231n/classifiers/linear_classifier.py
+++ assignment1/cs231n/classifiers/linear_classifier.py
@@ -57,7 +57,9 @@ class LinearClassifier(object):
             #########################################################################
             # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-            pass
+            inds = np.random.choice(num_train, batch_size)
+            X_batch = X[inds,:]
+            y_batch = y[inds]
 
             # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
@@ -72,7 +74,7 @@ class LinearClassifier(object):
             #########################################################################
             # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-            pass
+            self.W += - learning_rate * grad
 
             # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
@@ -102,8 +104,9 @@ class LinearClassifier(object):
         ###########################################################################
         # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-        pass
-
+        scores = np.matmul(X, self.W)
+        y_pred = np.argmax(scores, axis=1)
+        
         # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
         return y_pred
 
--- assignment1/cs231n/classifiers/softmax.py
+++ assignment1/cs231n/classifiers/softmax.py
@@ -33,9 +33,30 @@ def softmax_loss_naive(W, X, y, reg):
     #############################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-    pass
+    num_classes = W.shape[1]
+    num_train = X.shape[0]
+    
+    for i in range(num_train):
+        scores = X[i].dot(W)
+        scores -= np.max(scores)
+        exp_scores = np.exp(scores)
+        correct_class_score = exp_scores[y[i]]
+        exp_scores_sum = np.sum(exp_scores)
+        loss -= np.log(correct_class_score / exp_scores_sum)
+        
+        for j in range(num_classes):
+            if j == y[i]:
+                dW[:,j] -= X[i]*(exp_scores_sum-correct_class_score)/exp_scores_sum
+            else:
+                dW[:,j] += X[i]*exp_scores[j]/exp_scores_sum
+
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+    loss /= num_train
+    loss += reg * np.sum(W * W)
+    
+    dW /= num_train 
+    dW += 2 * reg * W
 
     return loss, dW
 
@@ -58,7 +79,22 @@ def softmax_loss_vectorized(W, X, y, reg):
     #############################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-    pass
+    num_train = X.shape[0]
+    scores = X.dot(W)
+    scores -= np.argmax(scores, axis=1)[:,np.newaxis]
+    exp_scores = np.exp(scores)
+    correct_class_score = exp_scores[np.arange(num_train),y]
+    exp_scores_sum = np.sum(exp_scores, axis=1)
+    
+    loss = np.sum(-np.log(correct_class_score / exp_scores_sum))
+    loss /= num_train
+    loss += reg * np.sum(W * W)
+    
+    exp_scores[np.arange(num_train),y] = correct_class_score - exp_scores_sum
+    exp_scores /= exp_scores_sum[:,np.newaxis]
+    dW = np.matmul(X.T, exp_scores)
+    dW /= num_train 
+    dW += 2 * reg * W
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
